6f05baf4b199d3c603b3751dbfa62761d993edcc
mabasset <matte.bassetto01@gmail.com>
2026-08-20 08:27:27 +0000

    ft_putstr_nl const arg

--- ft_putstr_nl.c
+++ ft_putstr_nl.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
 
-void    ft_putstr_nl(char *s)
-{
-    while (*s != '\0')
-        write(1, s++, 1);
-    write(1, "\n", 1);
+void ft_putstr_nl(const char* s) {
+  while (*s != '\0')
+    write(1, s++, 1);
+  write(1, "\n", 1);
 }
